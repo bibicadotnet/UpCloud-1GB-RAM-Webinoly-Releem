532e07492fcb2d6abf85c59c5a26fead1bcf432f
bibicadotnet <119997727+bibicadotnet@users.noreply.github.com>
2023-09-12 03:45:16 +0700

    Update releem.sh

--- releem.sh
+++ releem.sh
@@ -1,1 +1,8 @@
-
+#!/bin/bash
+mysql
+CREATE USER 'releem'@'%' identified by '[Password]';
+GRANT PROCESS, REPLICATION CLIENT, SHOW VIEW ON *.* TO 'releem'@'%';
+GRANT SELECT ON performance_schema.events_statements_summary_by_digest TO 'releem'@'%';
+FLUSH PRIVILEGES;
+EXIT;
+yes y| RELEEM_MYSQL_PASSWORD='[Password]' RELEEM_MYSQL_LOGIN='releem' RELEEM_MYSQL_MEMORY_LIMIT=0 RELEEM_API_KEY=c734e3de-3b21-4c29-96c4-26f3cdaf902f RELEEM_CRON_ENABLE=1 bash -c "$(curl -L https://releem.s3.amazonaws.com/v2/install.sh)"
