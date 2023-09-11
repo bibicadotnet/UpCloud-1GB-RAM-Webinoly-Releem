b8878adc93e8d751bfdeaff192a2c4ae2eb2e3d2
bibicadotnet <119997727+bibicadotnet@users.noreply.github.com>
2023-09-12 04:07:03 +0700

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -7,12 +7,13 @@ sudo wget --no-check-certificate https://raw.githubusercontent.com/bibicadotnet/
 sudo service mysql restart
 
 # setup releem
-mysql
+mysql 2>/dev/null <<EOF
 CREATE USER 'releem'@'%' identified by '[Password]';
 GRANT PROCESS, REPLICATION CLIENT, SHOW VIEW ON *.* TO 'releem'@'%';
 GRANT SELECT ON performance_schema.events_statements_summary_by_digest TO 'releem'@'%';
 FLUSH PRIVILEGES;
 EXIT;
+EOF
 yes y| RELEEM_MYSQL_PASSWORD='[Password]' RELEEM_MYSQL_LOGIN='releem' RELEEM_MYSQL_MEMORY_LIMIT=0 RELEEM_API_KEY=c734e3de-3b21-4c29-96c4-26f3cdaf902f RELEEM_CRON_ENABLE=1 bash -c "$(curl -L https://releem.s3.amazonaws.com/v2/install.sh)"
 
 # setup wp-cli
