6206cf2a627d4459f89d6de2c8ebce7ad00788a9
bibicadotnet <119997727+bibicadotnet@users.noreply.github.com>
2023-09-13 03:18:12 +0700

    Update releem.sh

--- releem.sh
+++ releem.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
+#setup releem
 mysql 2>/dev/null <<EOF
 CREATE USER 'releem'@'%' identified by '[Password]';
 GRANT PROCESS, REPLICATION CLIENT, SHOW VIEW ON *.* TO 'releem'@'%';
@@ -7,3 +8,8 @@ FLUSH PRIVILEGES;
 EXIT;
 EOF
 yes y| RELEEM_MYSQL_PASSWORD='[Password]' RELEEM_MYSQL_LOGIN='releem' RELEEM_MYSQL_MEMORY_LIMIT=0 RELEEM_API_KEY=c734e3de-3b21-4c29-96c4-26f3cdaf902f RELEEM_CRON_ENABLE=1 bash -c "$(curl -L https://releem.s3.amazonaws.com/v2/install.sh)"
+
+# setup crontab cho releem
+crontab -l > releem
+echo "0 3 * * * yes y| /bin/bash /opt/releem/mysqlconfigurer.sh -a" >> releem
+crontab releem
