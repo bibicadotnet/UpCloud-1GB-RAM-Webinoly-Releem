2e76479eeeaae1cf14bc019f12cb1d5113ea9770
bibicadotnet <119997727+bibicadotnet@users.noreply.github.com>
2023-09-16 18:25:23 +0700

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,6 +1,17 @@
 #!/bin/bash
-# setup Webinoly php 7.4
-sudo wget --no-check-certificate https://raw.githubusercontent.com/bibicadotnet/Webinoly-Optimization/master/webinoly74.sh -O webinoly_mod.sh && sudo chmod +x webinoly_mod.sh && sudo ./webinoly_mod.sh
+locale-gen en_US.UTF-8
+export LANG=en_US.UTF-8
+export LC_ALL=en_US.UTF-8
+sudo apt update -y
+sudo apt install zip -y
+sudo apt install unzip -y
+sudo apt install htop -y
+sudo apt install screen -y
+
+sudo wget --no-check-certificate https://raw.githubusercontent.com/bibicadotnet/webinoly/master/weby -O weby && sudo chmod +x weby && sudo ./weby -clean
+sudo rm /opt/webinoly/webinoly.conf
+sudo wget --no-check-certificate https://raw.githubusercontent.com/bibicadotnet/Webinoly-Optimization/master/webinoly_php74.conf -O /opt/webinoly/webinoly.conf
+sudo stack -lemp -build=light
 
 # setup mysql
 sudo wget --no-check-certificate https://raw.githubusercontent.com/bibicadotnet/VPS-1GB-RAM-Webinoly-Releem/main/mysql_3G_ram.conf -O /etc/mysql/my.cnf
